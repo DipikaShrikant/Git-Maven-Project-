88892bb252405b141449a37f758f65ba186b823d
DipikaShrikant <165634072+DipikaShrikant@users.noreply.github.com>
2024-04-01 13:10:46 +0530

    Update Git_Demo_1.java

--- TB_24_Git_Demo_Project/src/test/java/DemoGit/Git_Demo_1.java
+++ TB_24_Git_Demo_Project/src/test/java/DemoGit/Git_Demo_1.java
@@ -4,6 +4,11 @@ public class Git_Demo_1 {
 
 	public static void main(String[] args) {
 		System.out.println("This is Git");
+
+		for(int i=0;i<=5;i++){
+					System.out.println("*");
+
+		}
 	}
 
 }
