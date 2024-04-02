d56f0320843c8cfb868809fdbaf314b883983da0
DipikaShrikant <165634072+DipikaShrikant@users.noreply.github.com>
2024-04-02 11:44:51 +0530

    Update Git_Demo_1.java

--- TB_24_Git_Demo_Project/src/test/java/DemoGit/Git_Demo_1.java
+++ TB_24_Git_Demo_Project/src/test/java/DemoGit/Git_Demo_1.java
@@ -1,11 +1,11 @@
 package DemoGit;
-public void m1(){
-	System.out.println("==========M1 method =======");
 
-}
 	
 public class Git_Demo_1 {
+public void m2(){
+	System.out.println("==========M2 method =======");
 
+}
 	public static void main(String[] args) {
 		
 System.out.println("I am using the branch of Git");
