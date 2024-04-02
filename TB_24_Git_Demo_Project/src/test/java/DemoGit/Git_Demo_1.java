0d577802eff04ee3868148ed27bbabfdac98dd50
Dipika Chincholkar <chincholkardipika@gmail.com>
2024-04-02 10:44:44 +0530

    New Update

--- TB_24_Git_Demo_Project/src/test/java/DemoGit/Git_Demo_1.java
+++ TB_24_Git_Demo_Project/src/test/java/DemoGit/Git_Demo_1.java
@@ -3,12 +3,9 @@ package DemoGit;
 public class Git_Demo_1 {
 
 	public static void main(String[] args) {
-		System.out.println("This is Git");
+		
 
-		for(int i=0;i<=5;i++){
-					System.out.println("*");
-
-		}
+		
 	}
 
 }
